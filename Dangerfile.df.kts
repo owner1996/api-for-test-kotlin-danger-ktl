e1658206d9fb6edaa8fa6d7918e89d968de431ad
Mukhammad Zhalolov <muhammadzalolov@gmail.com>
2023-03-29 15:20:48 +0300

    commit #4

--- Dangerfile.df.kts
+++ Dangerfile.df.kts
@@ -13,23 +13,23 @@ register plugin DetektPlugin
 
 danger(args) {
 
-//    val allSourceFiles = git.modifiedFiles + git.createdFiles
-//    val changelogChanged = allSourceFiles.contains("CHANGELOG.md")
-//    val sourceChanges = allSourceFiles.firstOrNull { it.contains("src") }
-//
-//    onGitHub {
-//        val isTrivial = pullRequest.title.contains("#trivial")
-//
-//        // Big PR Check
-//        if ((pullRequest.additions ?: 0) - (pullRequest.deletions ?: 0) > 300) {
-//            warn("Big PR, try to keep changes smaller if you can")
-//        }
-//
-//        // Work in progress check
-//        if (pullRequest.title.contains("WIP", false)) {
-//            warn("PR is classed as Work in Progress")
-//        }
-//    }
+  danger  val allSourceFiles = git.modifiedFiles + git.createdFiles
+    val changelogChanged = allSourceFiles.contains("CHANGELOG.md")
+    val sourceChanges = allSourceFiles.firstOrNull { it.contains("src") }
+
+    onGitHub {
+        val isTrivial = pullRequest.title.contains("#trivial")
+
+        // Big PR Check
+        if ((pullRequest.additions ?: 0) - (pullRequest.deletions ?: 0) > 300) {
+            warn("Big PR, try to keep changes smaller if you can")
+        }
+
+        // Work in progress check
+        if (pullRequest.title.contains("WIP", false)) {
+            warn("PR is classed as Work in Progress")
+        }
+    }
 
     val detektReports = Files.find(Paths.get(""), 10, BiPredicate { path, attributes ->
         val fileName = path.toFile().name
