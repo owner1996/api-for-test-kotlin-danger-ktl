a31843c6d9be316c8ddd143526b4443c2696c2a4
Mukhammad Zhalolov <muhammadzalolov@gmail.com>
2023-03-29 17:11:15 +0300

    refactor

--- src/main/kotlin/ru/jalolov/services/apifortestkotlindangerktl/ApiForTestKotlinDangerKtlApplication.kt
+++ src/main/kotlin/ru/jalolov/services/apifortestkotlindangerktl/ApiForTestKotlinDangerKtlApplication.kt
@@ -1,25 +1,11 @@
 package ru.jalolov.services.apifortestkotlindangerktl
 
-//import ru.jalolov.services.apifortestkotlindangerktl.plugin.DetektPlugin
-import io.github.ackeecz.danger.detekt.DetektPlugin
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import java.io.File
-import java.nio.file.Files
-import java.nio.file.Paths
-import java.util.function.BiPredicate
-import java.util.stream.Collectors
 
 @SpringBootApplication
 class ApiForTestKotlinDangerKtlApplication
 
 fun main(args: Array<String>) {
-	val detektReports = Files.find(Paths.get(""), 10, BiPredicate { path, _ ->
-		val fileName = path.toFile().name
-		fileName.endsWith("detekt.xml")
-	}).map { it.toFile() }.collect(Collectors.toList())
-
-	DetektPlugin.parseAndReport(*detektReports.toTypedArray())
-
 	runApplication<ApiForTestKotlinDangerKtlApplication>(*args)
 }
